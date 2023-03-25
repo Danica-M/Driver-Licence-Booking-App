1391b93741f9765aa41e61bf8fde7356ac1e9d15
danica <danica.montano@outlook.com>
2023-03-25 15:18:16 +1300

    Homepage creation

---
+++ app/src/main/java/com/example/nzta_booking_app/Normal_Home.java
@@ -0,0 +1,14 @@
+package com.example.nzta_booking_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Normal_Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.normal_home);
+    }
+}
